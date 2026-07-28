9b6a6d1d14ec6452237aa2ee2aca454ec059bc8f
dogukandireksiz <dogukandireksiz48@gmail.com>
2026-07-28 15:18:02 +0300

    feat: Created a new vehicle add to garage (POST) endpoint.

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"log"
-	
+	"fmt"
+	"log" // hata/bilgi yazdırma kütüphanesidir.
 
-	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3" //web sunucu altyapısı.
 )
 
 // Veri modeli
@@ -29,12 +29,41 @@ var garage = []Car{
 
 func main()  {
 	
-	app := fiber.New()
+	app := fiber.New() //Fiber kütüphanesinden yeni bir sunucu nesnesi yaratılır. app değişkeni artık bizim web sunucumuzdur.
+
 
 	//Araçları listeleme
 	app.Get("/api/cars",func(c fiber.Ctx) error  {
 		//garage listesini JSON formatına çevirip istemciye gönderme
 		return c.JSON(garage)
+	})
+
+	// POST : Garaja yeni araç ekleme
+	app.Post("api/cars",func(c fiber.Ctx) error  {
+		// içi boş bir araba nesnesi yaratıyoruz.
+		var newCar Car
+
+		// istemciden gelen JSON verisini newCar'ın içine kopyalıyoruz. (Bind)
+		// hata çıkarsa (örn: JSON formatı bozuksa) if bloğu çalışır
+		// c.Bind().JSON(&newCar): Fiber v3'ün veri bağlama metodudur. İstemciden gelen JSON verisini okur ve bizim Car yapımızla eşleştirir.
+
+		if err := c.Bind().JSON(&newCar); err != nil{
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error" : "Gelen veri okunamadı veya fotmat hatali",
+			})
+		}
+
+		// Henüz gerçek bir veritabanı olmadığı için ID yi manuel veriyoruz
+		// Mevcut garaj uzunluğuna 1 ekleyip string e çeviriyoruz
+		newCar.ID = fmt.Sprintf("%d",len(garage)+1)
+
+		// Yeni aracı garaj listesine ekliyoruz
+		garage = append(garage, newCar)
+
+		// 201 Created statü koduyla birlikte eklenen aracı geri dönüyoruz
+		return c.Status(fiber.StatusCreated).JSON(newCar)
+		//c.Status(fiber.StatusCreated): Sadece veri dönmek yetmez, HTTP iletişim kurallarına uymalıyız. Başarılı bir ekleme işlemi yapıldığında standart olarak 201 Created kodu dönülür. Hata durumunda ise 400 Bad Request döndük.
+		
 	})
 
 	log.Println("Garaj API 3000 portunda çalışıyor...")
